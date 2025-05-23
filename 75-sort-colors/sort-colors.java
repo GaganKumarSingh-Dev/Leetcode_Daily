56f80d7c27a9b4c048f0c921181de9371182c1d1
Gagan Kumar Singh <154595655+GaganKumarSingh-Dev@users.noreply.github.com>
2025-05-23 14:18:25 +0530

    Time: 13 ms (2.61%) | Memory: 42.4 MB (9.94%) - LeetSync

--- 75-sort-colors/sort-colors.java
+++ 75-sort-colors/sort-colors.java
@@ -1,43 +1,31 @@
 class Solution {
     public void sortColors(int[] nums) {
-        int[] countArr = { 0, 0, 0 };
+        int low = 0, mid = 0, high = nums.length - 1; // 3 pointers
 
-        for (int i = 0; i < nums.length; i++) {
-            switch (nums[i]) {
-                case 0:
-                    countArr[0]++;
-                    break;
-                case 1:
-                    countArr[1]++;
-                    break;
-                case 2:
-                    countArr[2]++;
-                    break;
-                default:
-                    break;
-            }
-        }
+        while (mid <= high) {
+            if (nums[mid] == 0) {
+                // swapping arr[low] and arr[mid]
+                int temp = nums[low];
+                nums[low] = nums[mid];
+                nums[mid] = temp;
 
-        for (int j = 0; j < nums.length; j++) {
-            if (countArr[0] > 0) {
-                nums[j] = 0;
-                countArr[0]--;
-                continue;
-            } else if (countArr[1] > 0) {
-                nums[j] = 1;
-                countArr[1]--;
-                continue;
-            } else if (countArr[2] > 0) {
-                nums[j] = 2;
-                countArr[2]--;
-                continue;
+                low++;
+                mid++;
+
+            } else if (nums[mid] == 1) {
+                mid++;
+
             } else {
-                System.out.println("Array is empty");
+                // swapping arr[mid] and arr[high]
+                int temp = nums[mid];
+                nums[mid] = nums[high];
+                nums[high] = temp;
+
+                high--;
             }
         }
 
-        for (int n : nums)
-            System.out.print(n + " ");
+        for(int k: nums) System.out.print(k + " ");
 
     }
 
